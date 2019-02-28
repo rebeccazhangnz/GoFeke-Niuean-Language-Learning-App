fcfcf561d4751f26ac4c80509fc91e703886ec2d
Grantis <grantrigby1992@gmail.com>
2019-02-28 18:38:49 +1300

    added db functions

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const server = express()
 
 const fekeRoute = require('./routes/feke')
-const statusRoute = require('./routes/status')
-const categoryRoute = require('./routes/category')
+// const statusRoute = require('./routes/status')
+const familyRoute = require('./routes/family')
 // import Routes here
 
 server.use(express.json())
@@ -12,8 +12,8 @@ server.use(express.static(path.join(__dirname, './public')))
 
 // placeholder for api
 server.use('/api/v1/feke', fekeRoute)
-server.use('/api/v1/status', statusRoute)
-server.use('/api/v1/category/family', categoryRoute)
+// server.use('/api/v1/status', statusRoute)
+server.use('/api/v1/category/family', familyRoute)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
