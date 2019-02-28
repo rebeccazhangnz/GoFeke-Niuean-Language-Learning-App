1c79a74f55604d25b1de030717c6f64b692d3fb1
Micah Nathaniel Titus Peautagi <micahpeautagi@gmail.com>
2019-02-28 19:20:17 +1300

    Created Server side food routes

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const express = require('express')
 const server = express()
 
 const fekeRoute = require('./routes/feke')
-//const statusRoute = require('./routes/status')
+// const statusRoute = require('./routes/status')
 const greetingsRoute = require('./routes/greetings')
-const categoryRoute = require('./routes/category')
 const familyRoute = require('./routes/family')
+const foodRoute = require('./routes/food')
 // import Routes here
 
 server.use(express.json())
@@ -14,14 +14,13 @@ server.use(express.static(path.join(__dirname, './public')))
 
 // placeholder for api
 server.use('/api/v1/feke', fekeRoute)
-//server.use('/api/v1/status', statusRoute)
+// server.use('/api/v1/status', statusRoute)
 server.use('/api/v1/category/greetings', greetingsRoute)
-server.use('/api/v1/category/family', categoryRoute)
 server.use('/api/v1/category/family', familyRoute)
+server.use('/api/v1/category/food', foodRoute)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 module.exports = server
-
