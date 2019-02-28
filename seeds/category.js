b47ac480ab9d0d4b05e52d7c91eba3909dc6ab65
ngapeita <ngapeita@.com>
2019-02-28 15:30:20 +1300

    filled content seed

--- seeds/category.js
+++ seeds/category.js
@@ -1,14 +1,13 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('category').del()
     .then(function () {
       // Inserts seed entries
       return knex('category').insert([
         {id: 1, name: 'Family'},
-        {id: 2, name: 'rowValue2'},
-        {id: 3, name: 'rowValue3'},
-        {id: 3, name: 'rowValue3'}
-      ]);
-    });
-};
+        {id: 2, name: 'Food and drink'},
+        {id: 3, name: 'Greetings'},
+        {id: 4, name: 'Numbers'}
+      ])
+    })
+}
