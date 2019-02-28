b4adeb23e8977f4dee1ab5ded0fe708484aee06e
Grantis <grantrigby1992@gmail.com>
2019-02-28 15:27:49 +1300

    initial welcome page

---
+++ client/components/Welcome.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+class Welcome extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            translate: 'Kamata'
+        }
+    }
+
+
+
+    translate = () => {
+        this.setState({
+            translate: 'Start'
+        })
+    }
+
+    revert = () => {
+        this.setState({
+            translate: 'Kamata'
+        })
+    }
+
+    render() {
+        return (
+            <div className='welcome-page'>
+                <h1 className='welcome-title'>Feke Welcomes you</h1>
+                <input type='button' value={this.state.translate} onMouseEnter={() => this.translate()}
+                    onMouseLeave={() => this.revert()} />
+            </div>
+        )
+    }
+}
+
+export default Welcome
