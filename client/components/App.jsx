a41c53ea4539b6827536bb23b6b711ed6b266ee9
Grantis <grantrigby1992@gmail.com>
2019-02-28 14:30:48 +1300

    initial react app.js

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+// initial App.js file, subject to change
+
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={placeholder-component} />
+    </Switch>
+  )
+}
+
+export default App
