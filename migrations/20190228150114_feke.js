a647baf4c977d6e318a4d5eec33961b3bf834370
ngapeita <ngapeita@.com>
2019-02-28 15:19:32 +1300

    Created DB tables

---
+++ migrations/20190228150114_feke.js
@@ -0,0 +1,14 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('feke', function (table) {
+    table.increments('id').primary()
+    table.string('category_id').references('category.id')
+    table.string('name')
+    table.string('village')
+    table.string('image')
+    table.string('status')
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('feke')
+}
