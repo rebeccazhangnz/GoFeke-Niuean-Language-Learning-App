1dff5ba72f51036ea0e4a9cb502fd4890734b096
Grantis <grantrigby1992@gmail.com>
2019-02-28 17:17:45 +1300

    create app component routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Home from './Home'
-
-
+// import Welcome from './Welcome'
+import CreateFeke from './CreateFeke'
+import Study from './Study'
+// import Quiz from './Quiz'
+ 
 const App = () => {
   return (
     <Switch>
-      <Route exact path="/welcome" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route exact path='/welcome' component={Welcome} />
+      <Route exact path='/feke' component={CreateFeke} />
+      <Route exact path='/studymaterial/:category' component={Study} />
+      {/*<Route exact path='/quiz/:category' component={Quiz} />*/}
     </Switch>
   )
 }
