e80ea2bd29c8dee062a704e0f28895878da21b26
Grantis <grantrigby1992@gmail.com>
2019-02-28 17:46:39 +1300

    updated feke db function

--- server/routes/feke.js
+++ server/routes/feke.js
@@ -3,6 +3,18 @@ const db = require('../db/feke')
 const router = express.Router()
 
 // This api gets the feke from the database
+
+router.get('/', (req, res) => {
+  db.getFeke()
+    .then(feke => {
+      res.json(feke)
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+
 router.post('/', (req, res) => {
     db.createFeke()
       .then(feke => {
