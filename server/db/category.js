ec57702465ae75c3dc789d9b4f74be98d332c90f
Grantis <grantrigby1992@gmail.com>
2019-02-28 14:42:15 +1300

    initial category db file

---
+++ server/db/category.js
@@ -0,0 +1,14 @@
+const connection = require('.')
+
+function /* */ (db = connection) {
+//   return db(/* placeholder table */)
+// sample of a join between two tables, for informational purpose only
+//     .join('modules', 'modules.id', 'module_id')
+//     .join('exercises', 'assessments.id', 'assessment_id')
+//     .select('modules.id as moduleId', 'modules.title as moduleTitle', 'assessments.id as assessmentsId', 'assessments.title as assessmentsTitle', 'assessments.description', 'assessments.link', 'assessments.week_day', 'exercises.id as exerciseId', 'exercises.title as exerciseTitle')
+}
+
+
+module.exports = {
+ // placeholder function
+}
