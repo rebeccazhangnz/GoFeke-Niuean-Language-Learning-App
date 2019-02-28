32ef74726e323f1eee6b2468ba55e4fbba347750
Grantis <grantrigby1992@gmail.com>
2019-02-28 18:25:47 +1300

    updated basic styles

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -232,6 +232,7 @@ function (_React$Component) {
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
         className: "welcome-title"
       }, "Feke Welcomes you"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+        className: "welcome-btn",
         type: "button",
         value: this.state.translate,
         onMouseEnter: function onMouseEnter() {
