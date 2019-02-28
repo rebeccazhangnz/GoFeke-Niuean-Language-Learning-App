3b003fc884f92be29e13495f7130f1a94e6fbcd1
Grantis <grantrigby1992@gmail.com>
2019-02-28 15:42:35 +1300

    webpack update

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import Welcome from './Welcome'
 // initial App.js file, subject to change
 
 
 const App = () => {
   return (
     <Switch>
-      <Route exact path="/" component={placeholder-component} />
+      <Route exact path="/" component={Welcome} />
     </Switch>
   )
 }
