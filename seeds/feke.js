b7c5896178d39d104dca4b7aefb7c2b7550b2d72
ngapeita <ngapeita@.com>
2019-02-28 15:33:02 +1300

    filled all seed

--- seeds/feke.js
+++ seeds/feke.js
@@ -1,13 +1,11 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('feke').del()
     .then(function () {
       // Inserts seed entries
       return knex('feke').insert([
-        {id: 1, name: 'rowValue1', village: '', },
-        {id: 2, name: 'rowValue2'},
-        {id: 3, name: 'rowValue3'}
-      ]);
-    });
-};
+        {id: 1, name: 'Fetufeke', village: 'Makefu'}
+
+      ])
+    })
+}
