606d5cba71e9ccfb3dce6f0b0c3c50f7ec93cf7a
Yan Fang <yanfang196888@gmail.com>
2019-03-02 11:54:54 +1300

    action for category

--- client/Actions/index.js
+++ client/Actions/index.js
@@ -1,8 +1,14 @@
-import {getCategory} from '../api/category'
+import {getCategory, getCategoryList} from '../api/category'
 
 function requestContent () {
   return {
     type: 'REQUESTING_CONTENT'
+  }
+}
+
+function requestCategoryList () {
+  return {
+    type: 'REQUESTING_CATEGORY_LIST'
   }
 }
 
@@ -13,12 +19,29 @@ function receiveContent (content) {
   }
 }
 
+function receiveCategoryList (list) {
+  return {
+    type: 'RECEIVING_CATEGORY_LIST',
+    categoryList: list
+  }
+}
+
 export function fetchCategory () {
   return function (dispatch) {
     dispatch(requestContent())
     getCategory(name)
       .then(content => {
         dispatch(receiveContent(content))
+      })
+  }
+}
+
+export function fetchCategoryList () {
+  return function (dispatch) {
+    dispatch(requestCategoryList())
+    getCategoryList()
+      .then(list => {
+        dispatch(receiveCategoryList(list))
       })
   }
 }
