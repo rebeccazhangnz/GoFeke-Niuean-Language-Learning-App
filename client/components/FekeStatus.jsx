0210d60e402555adb35b75433cccca0747f044dc
Haixin Zhang <rebeccazhangnz@outlook.com>
2019-03-02 12:10:28 +1300

    need to work on other branch temp save

--- client/components/FekeStatus.jsx
+++ client/components/FekeStatus.jsx
@@ -1,1 +1,18 @@
-import
+import React from 'react'
+import {connect} from 'react-redux'
+
+class FekeStatus extends React.Component {
+  // componentDidMount () {
+  //   this.props.dispatch(getFeke())
+  // }
+
+  render () {
+    return (
+      <div className = 'fekeStatus'>
+        <h1>This is the fekeStatus component</h1>
+      </div>
+    )
+  }
+}
+
+export default FekeStatus
