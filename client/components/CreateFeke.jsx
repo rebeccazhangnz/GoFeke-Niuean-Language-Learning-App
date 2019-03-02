fcc7f1378d2d82905365fd42580976c7c697b696
Haixin Zhang <rebeccazhangnz@outlook.com>
2019-03-02 20:29:17 +1300

    added event handler

--- client/components/CreateFeke.jsx
+++ client/components/CreateFeke.jsx
@@ -1,39 +1,57 @@
 import React from 'react'
 
 class CreateFeke extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { type: '', name: '', village: '' };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
+constructor(props) {
+  super(props)
+  this.state = { image: '', name: '', village: '' }
 
 
+  this.handleSubmit = this.handleSubmit.bind(this)
+  this.handleOnclick = this.handleOnclick.bind(this)
+  this.handleDropdown = this.handleDropdown.bind(this)
+  this.handleChange = this.handleChange.bind(this)
+}
 
-  handleSubmit = () => {
-    // this.setState({value: event.target.value});
-    alert(this.state.village)
-  }
+
+handleOnclick(e){
+this.setState({
+  image : e.target.name
+})
+}
+
+handleDropdown(e) {
+  this.setState({ 
+    village: e.target.value
+  })
+}
+
+handleChange(e) {
+  this.setState({ 
+    name: e.target.value
+  })
+}
 
 
-  render() {
-    return (
-      <div className='createfeke-page'>
-        <form onSubmit={this.handleSubmit}>
-        <img className="feke-image" src = "images/feke-blue.png" />
-        <img className="feke-image" src = "images/feke-green.png" />
-        <img className="feke-image" src = "images/feke-purple.png" />
-        <img className="feke-image" src = "images/feke-red.png" />
-          <label htmlFor="name">Name </label>
-          <input type="text" name='name' value={this.state.name} onChange={this.handleChange} />
+handleSubmit = () => {
+  alert('name : '+ this.state.name + '  village : ' + this.state.village + '  image : ' + this.state.image)
+}
 
-          <label htmlFor="village">Village </label>
-          <select village={this.state.village} onChange={this.handleChange}>
+
+render() {
+  return (
+    <div className='createfeke-page'>
+      <form onSubmit={this.handleSubmit}>
+
+      <img className="feke-image" src = "images/feke-blue.png" name = 'feke-blue.png' onClick = {this.handleOnclick}/>
+      <img className="feke-image" src = "images/feke-green.png" name = 'feke-green.png' onClick = {this.handleOnclick}/>
+      <img className="feke-image" src = "images/feke-purple.png" name = 'feke-purple.png' onClick = {this.handleOnclick}/>
+      <img className="feke-image" src = "images/feke-red.png" name = 'feke-red.png' onClick = {this.handleOnclick}/>
+
+      <label htmlFor="name">Name </label>
+        <input type="text" name='name' value={this.state.name} onChange={this.handleChange} />
+
+      <label htmlFor="village">Village </label>
+        <select value = {this.state.village} onChange={this.handleDropdown}>
           <option village="alofi">Alofi</option>
           <option village="avatele">Avatele</option>
           <option village="hakupu">Hakupu</option>
@@ -47,12 +65,12 @@ class CreateFeke extends React.Component {
           <option village="toi">Toi</option>
           <option village="tuapa">Tuapa</option>
           <option village="vaiea">Vaiea</option>
-          </select>
-          <input type="submit" value="Create feke" />
-        </form>
-      </div>
-    )
-  }
+        </select>
+        <input type="submit" value="Create feke" />
+      </form>
+    </div>
+  )
+}
 }
 
 export default CreateFeke
