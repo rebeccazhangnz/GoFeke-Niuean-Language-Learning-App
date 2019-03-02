a99a3666b99d0d46fb650b6dbb711770472c47f3
Haixin Zhang <rebeccazhangnz@outlook.com>
2019-03-02 17:51:13 +1300

    init commit

--- client/components/CreateFeke.jsx
+++ client/components/CreateFeke.jsx
@@ -1,21 +1,58 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
-const CreateFeke = () => {
-  return (
-    <div>
-      <p>Name your feke and your village</p>
-        <form>
-        <label htmlFor="name">Name</label>
-        <input type="text" />
-        <label htmlFor="village">Village</label>
-        <input type="text" />
-        <input type="submit" value="Create feke" />
-      </form>
-    </div>
-  )
+class CreateFeke extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { type: '', name: '', village: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+
+
+  handleSubmit = () => {
+    // this.setState({value: event.target.value});
+    alert(this.state.village)
+  }
+
+
+  render() {
+    return (
+      <div className='createfeke-page'>
+        <form onSubmit={this.handleSubmit}>
+        <img className="feke-image" src = "images/feke-blue.png" />
+        <img className="feke-image" src = "images/feke-green.png" />
+        <img className="feke-image" src = "images/feke-purple.png" />
+        <img className="feke-image" src = "images/feke-red.png" />
+          <label htmlFor="name">Name </label>
+          <input type="text" name='name' value={this.state.name} onChange={this.handleChange} />
+
+          <label htmlFor="village">Village </label>
+          <select village={this.state.village} onChange={this.handleChange}>
+          <option village="alofi">Alofi</option>
+          <option village="avatele">Avatele</option>
+          <option village="hakupu">Hakupu</option>
+          <option village="hikutavake">Hikutavake</option>
+          <option village="lakepa">Lakepa</option>
+          <option village="liku">Liku</option>
+          <option village="makefu">Makefu</option>
+          <option village="mutalau">Mutalau</option>
+          <option village="namukulu">Namukulu</option>
+          <option village="tamakautoga">Tamakautoga</option>
+          <option village="toi">Toi</option>
+          <option village="tuapa">Tuapa</option>
+          <option village="vaiea">Vaiea</option>
+          </select>
+          <input type="submit" value="Create feke" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default CreateFeke
-
-// handleSubmit will need to be defined.
