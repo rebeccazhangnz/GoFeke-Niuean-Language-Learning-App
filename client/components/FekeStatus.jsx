f0985017db268216ac227cedbf9745646b539b6e
Haixin Zhang <rebeccazhangnz@outlook.com>
2019-03-02 13:56:35 +1300

    created test component

---
+++ client/components/FekeStatus.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import {connect} from 'react-redux'
+
+class FekeStatus extends React.Component {
+  render () {
+    return (
+      <div className = 'fekeStatus'>
+        <h1>This is the Feke StatusComponent</h1>
+      </div>
+    )
+  }
+}
+
+export default FekeStatus
