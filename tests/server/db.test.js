2f5fd4ffc71cb0a0140834d839997b24f8c716c9
Haixin Zhang <rebeccazhangnz@outlook.com>
2019-03-04 17:56:16 +1300

    added data base test for getContent

--- tests/server/db.test.js
+++ tests/server/db.test.js
@@ -1,3 +1,25 @@
+const testEnv = require('./test-environment')
+const dbFeke = require('../../server/db/feke')
+const dbCategory = require('../../server/db/category')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = testEnv.getTestDb()
+  return testEnv.initialise(testDb)
+})
+
+afterEach(() => testEnv.cleanup(testDb))
 test('Test testing environment', () => {
   expect(true).toBeTruthy()
+})
+
+test('getUsers gets all catogory', () => {
+  const expected = 'family'
+  return dbCategory.getCategoryList(testDb)
+    .then(Category => {
+      const actual = Category[0].name
+      expect(actual).toBe(expected)
+    })
+    .catch(err => expect(err).toBeNull())
 })
