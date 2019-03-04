bd259c6b521982c9360d9c0c917b93d3f2259d4d
Jenny Fang <yanfang196888@gmail.com>
2019-03-04 14:10:28 +1300

    update answerOptions table

--- migrations/20190228150114_feke.js
+++ migrations/20190228150114_feke.js
@@ -1,7 +1,7 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('feke', function (table) {
     table.increments('id').primary()
-    table.string('category_id').references('category.id')
+    table.integer('category_id').references('category.id')
     table.string('name')
     table.string('village')
     table.string('image')
