94a274ff03772cce0f2dce17abb66898a9e1ff46
Yan Fang <yanfang196888@gmail.com>
2019-03-04 13:13:19 +1300

    Add header text

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       <Link to = {`/category`}>
-        <h1 className="header">GO Feke!</h1>
+        <h1 className="header">GO! Feke</h1>
       </Link>
     </div>
   )
