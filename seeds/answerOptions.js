203f13cfbb11237cb6d055e5423f81c19e43576b
Micah Nathaniel Titus Peautagi <micahpeautagi@gmail.com>
2019-03-05 11:37:14 +1300

    refactor

--- seeds/answerOptions.js
+++ seeds/answerOptions.js
@@ -12,7 +12,7 @@ exports.seed = function (knex, Promise) {
         { id: 5, questions_id: 5, option1: 'Fakamofa mahi mafu', option2: 'Fakaalofa lahi atu', option3: 'lahi atu', option4: 'Atu lahi faka' },
         { id: 6, questions_id: 6, option1: 'Koe atu', option2: 'Koe koe', option3: 'Koe kia', option4: 'Kiakia' },
         { id: 7, questions_id: 7, option1: 'Ua', option2: 'Tasi', option3: 'Tahi', option4: 'Taha' },
-        { id: 8, questions_id: 8, option1: 'Hogofulu', option2: 'Hogoulu', option3: 'Hogofulu', option4: 'fulufulu' },
+        { id: 8, questions_id: 8, option1: 'Hogofulu', option2: 'Hogoulu', option3: 'Hogomulu', option4: 'fulufulu' },
         { id: 9, questions_id: 9, option1: 'Mogo pogipogi', option2: 'Mongo pongi pongi', option3: 'Moninga', option4: 'Mafana' },
         { id: 10, questions_id: 10, option1: 'Afimafi', option2: 'Afiafi', option3: 'Tupou laa', option4: 'Pale laa' },
         { id: 11, questions_id: 11, option1: 'Mokomo', option2: 'Hoge', option3: 'Tatalu', option4: 'Momoko' },
