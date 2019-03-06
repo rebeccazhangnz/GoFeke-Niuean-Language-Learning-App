219e39cf7d588db8e20a06ba845e3b2c60d8a463
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-03-06 14:09:08 +1300

    added route tests for quiz and category

--- tests/server/routes.test.js
+++ tests/server/routes.test.js
@@ -1,10 +1,6 @@
 const server = require('../../server/server')
 const request = require('supertest')
 
-test('Test testing environment', () => {
-  expect(true).toBeTruthy()
-})
-
 test('Test PUT route "/api/v1/feke" ', done => {
   return request(server)
     .put('/api/v1/feke')
@@ -28,6 +24,62 @@ test('Test PUT route "/api/v1/feke" ', done => {
         status: 'Level 1'
       }])
       done()
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+test('Category /api/v1/category/family', () => {
+  return request(server)
+    .get('/api/v1/category/family')
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .then((res) => {
+      const content = res.body
+      expect(content[0].name).toBe('family')
+      expect(content[7].niuean).toBe('pulapulaola / mokopuna')
+      expect(content[12].english).toBe('parent')
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+
+test('Category /api/v1/category/food', () => {
+  return request(server)
+    .get('/api/v1/category/food')
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .then((res) => {
+      const content = res.body
+      expect(content[0].name).toBe('food')
+      expect(content[4].niuean).toBe('fua meleni')
+      expect(content[11].english).toBe('coconut')
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+
+test('quiz /api/v1/quiz', () => {
+  return request(server)
+    .get('/api/v1/quiz')
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .then((res) => {
+      const content = res.body
+      expect(content[0].answer).toBe( "Matua taane")
+      expect(content[1].answer).toBe( "Matua fifine")
+      expect(content[2].answer).toBe( "Lole")  
+    })
+    .catch(err => expect(err).toBeNull())
+})
+
+test('quiz /api/v1/quiz', () => {
+  return request(server)
+    .get('/api/v1/quiz')
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .then((res) => {
+      const actual = res.body.length
+      expect(actual).toBe(20)
     })
     .catch(err => expect(err).toBeNull())
 })
