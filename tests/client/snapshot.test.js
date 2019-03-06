2d31436193b379880a892ce58edaf9da9f485095
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-03-07 00:48:09 +1300

    updated snapshot tests due to changes in components.

--- tests/client/snapshot.test.js
+++ tests/client/snapshot.test.js
@@ -6,37 +6,62 @@ import { shallow, render } from 'enzyme'
 import Header from '../../client/components/Header'
 import Dashboard from '../../client/components/Dashboard'
 import CreateFeke from '../../client/components/CreateFeke'
+import FekeStatus from '../../client/components/FekeStatus'
 
+
+test('<Dashboard />', () => {
+  const mockStore = configureStore()({ isUpdated: false })
+  const wrap = shallow(
+    <Provider store={mockStore}>
+      <Dashboard />)
+    </Provider>
+  )
+  const component = wrap.text()
+
+  expect(renderer.create(component).toJSON()).toMatchSnapshot()
+})
 
 test('<CreateFeke />', () => {
-    const mockStore = configureStore()({ isUpdated: false })
-    const wrap = shallow(
-      <Provider store={mockStore}>
-        <CreateFeke />)
-      </Provider>
-    )
-    const component = wrap
-  
-    expect(renderer.create(component).toJSON()).toMatchSnapshot()
+  const mockStore = configureStore()({
+    feke: [
+      {
+        id: 1,
+        category_id: 1,
+        name: 'Fekefeke',
+        village: 'Avatele',
+        image: 'feke-blue.png',
+        status: 'Level 1'
+      }
+    ]
   })
+  const wrap = shallow(
+    <Provider store={mockStore}>
+      <CreateFeke />)
+    </Provider>
+  )
+  const component = wrap
+
+  expect(renderer.create(component).toJSON()).toMatchSnapshot()
+})
 
 
 
 test('<Header />', () => {
   const wrap = shallow(<Header />)
   const component = wrap.text()
-      expect(renderer.create(component).toJSON()).toMatchSnapshot()
+  expect(renderer.create(component).toJSON()).toMatchSnapshot()
 })
 
 
-test('<Dashboard />', () => {
-    const mockStore = configureStore()({ isUpdated: false })
-    const wrap = shallow(
-      <Provider store={mockStore}>
-        <Dashboard />)
-      </Provider>
-    )
-    const component = wrap.text()
-  
-    expect(renderer.create(component).toJSON()).toMatchSnapshot()
-  })
+
+test('<CreateFeke />', () => {
+  const mockStore = configureStore()({ isUpdated: false })
+  const wrap = shallow(
+    <Provider store={mockStore}>
+      <CreateFeke />)
+    </Provider>
+  )
+  const component = wrap
+
+  expect(renderer.create(component).toJSON()).toMatchSnapshot()
+})
