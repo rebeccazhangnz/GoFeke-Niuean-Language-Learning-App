3f9b057234a24693e54bd6a9b4f990e07aa7390d
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-03-06 14:08:39 +1300

    updated test setup

--- tests/setup-enzyme-env.js
+++ tests/setup-enzyme-env.js
@@ -2,3 +2,5 @@ import {configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({adapter: new Adapter()})
+
+jest.spyOn(global.console, 'log').mockImplementation(() => jest.fn())
